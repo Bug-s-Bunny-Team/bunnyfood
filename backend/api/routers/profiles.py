8f0bdeac3c226c4d701b8c321bef2f8115436331
Marco Volpato <marco@mvolpato.xyz>
2022-08-19 14:01:15 +0200

    api: add followed profiles endpoints

--- backend/api/routers/profiles.py
+++ backend/api/routers/profiles.py
@@ -20,16 +20,6 @@ def get_profiles():
 
 
 @router.get(
-    '/profiles/followed',
-    response_model=List[schemas.SocialProfile],
-    response_model_exclude_unset=True,
-    dependencies=[Depends(get_db)],
-)
-def get_followed_profiles():
-    return []
-
-
-@router.get(
     '/profiles/{profile_id}',
     response_model=schemas.SocialProfile,
     response_model_exclude_unset=True,
@@ -42,11 +32,43 @@ def get_profile(profile_id: int):
     return profile
 
 
+@router.get(
+    '/profiles/popular/{limit}',
+    response_model=List[schemas.SocialProfile],
+    response_model_exclude_unset=True,
+    dependencies=[Depends(get_db)],
+)
+def get_most_popular_profiles(limit: int):
+    if limit > 50:
+        raise HTTPException(
+            status_code=400, detail='Can provide at most 50 popular profiles'
+        )
+    return []
+
+
+@router.get(
+    '/profiles/followed',
+    response_model=List[schemas.SocialProfile],
+    response_model_exclude_unset=True,
+    dependencies=[Depends(get_db)],
+)
+def get_followed_profiles():
+    return []
+
+
 @router.post(
-    '/profiles/',
+    '/profiles/followed/',
     status_code=status.HTTP_201_CREATED,
     dependencies=[Depends(get_db)],
 )
-def add_profile(profile: schemas.SocialProfile):
+def follow_profile(profile: schemas.SocialProfile):
     p, created = models.SocialProfile.get_or_create(username=profile.username)
     return p
+
+
+@router.delete(
+    '/profiles/followed/',
+    dependencies=[Depends(get_db)],
+)
+def follow_profile(profile: schemas.SocialProfile):
+    return {}
