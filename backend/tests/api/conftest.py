6cb694e1d6578a2a44aad63de81cc5fdc135ed8d
Marco Volpato <marco@mvolpato.xyz>
2022-08-20 15:04:19 +0200

    api: tests finally working

--- backend/tests/api/conftest.py
+++ backend/tests/api/conftest.py
@@ -1,25 +1,27 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 
-from db import db
-from db.utils import create_all_tables, init_db
+from api.dependencies import get_db
 from api.main import app
+from api.models import Base
 
 
-@pytest.fixture
-def transaction():
-    init_db('user', 'password', 'localhost', 'bunnyfood_test')
-    with db.transaction() as txn:
-        create_all_tables()
-        yield txn
-        # txn.rollback()
+@pytest.fixture(name='session')
+def session_fixture():
+    engine = create_engine('postgresql://user:password@localhost/bunnyfood_test')
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    with SessionLocal() as db:
+        Base.metadata.create_all(engine)
+        yield db
 
 
-@pytest.fixture(scope='class')
-def api_client_class(request):
-    request.cls.api_client = TestClient(app)
+@pytest.fixture(name='api_client')
+def api_fixture(session: Session):
+    def get_db_override():
+        return session
 
-
-@pytest.fixture
-def api_client():
-    return TestClient(app)
+    app.dependency_overrides[get_db] = get_db_override
+    yield TestClient(app)
+    app.dependency_overrides.clear()
--- backend/tests/api/test_locations.py
+++ backend/tests/api/test_locations.py
@@ -1,54 +1,18 @@
-# from db import models, db
-# from db.local_utils import create_all_tables, init_db
-#
-#
-# def test_get_all_locations(api_client):
-#     locations_models = [
-#         models.Location(
-#             name='test1', description='some desc', lat=43.5, long=54.3, score=4.5
-#         ),
-#         models.Location(
-#             name='test2', description='some desc', lat=43.5, long=54.3, score=4.5
-#         ),
-#         models.Location(
-#             name='test3', description='some desc', lat=43.5, long=54.3, score=4.5
-#         ),
-#     ]
-#     models.Location.bulk_create(locations_models)
-#
-#     # locations = api_client.get('/locations')
-#     # print(locations.json())
-#     #
-#     # assert locations.status_code == 200
-#     # assert len(locations.json()) > 0
-#
-#
-# def test_get_location_by_id(api_client):
-#     location = api_client.get('/locations/666')
-#
-#     assert location.status_code == 404
+from sqlalchemy.orm import Session
 
-import unittest
-import pytest
+from api import models
 
 
-# @pytest.mark.usefixtures('api_client')
-# class LocationsTestCase(unittest.TestCase):
-#     def setUp(self):
-#         pass
-#
-#     def tearDown(self):
-#         pass
-#
-#     def test_get_location_by_id(self, transaction):
-#         location = self.api_client.get('/locations/666')
-#
-#         assert location.status_code == 404
-from db import db
-from db.utils import create_all_tables
+def test_get_all_locations(api_client, session: Session):
+    locations = [
+        models.Location(name='testlocation1', description='some desc', score=1),
+        models.Location(name='testlocation2', description='some desc', score=1),
+        models.Location(name='testlocation3', description='some desc', score=1)
+    ]
 
+    session.bulk_save_objects(locations)
+    response = api_client.get('/locations')
+    session.rollback()
 
-def test_get_location_by_id(api_client, transaction):
-        location = api_client.get('/locations/666')
-
-        assert location.status_code == 404
+    assert response.status_code == 200
+    assert len(response.json()) == 3
