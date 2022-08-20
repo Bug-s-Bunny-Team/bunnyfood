a4074aa0ce4f69b725a6b47f1907a72ac34eb6bb
Tommaso Di Fant <jackiedifant@gmail.com>
2022-08-20 12:40:02 +0200

    #3 .

--- frontend/src/pages/Root.svelte
+++ frontend/src/pages/Root.svelte
@@ -13,8 +13,12 @@
     }
 </script>
 
-<strong class="link"><link name="Cognito Login" href="https://aws.amazon.com/it/cognito/"/></strong>
+<a role="button" href="https://aws.amazon.com/it/cognito/">Cognito Login</a>
 <button on:click={login}>Automatic Login</button>
 
 <style>
+    a {
+        display: block;
+        margin: 2em 0em;
+    }
 </style>
