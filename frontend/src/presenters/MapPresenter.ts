5f2a2e81a85597c05d91eeb6bfa9209009c8d280
Tommaso Di Fant <jackiedifant@gmail.com>
2022-08-20 12:38:10 +0200

    map popup label trial, sh*t but working aggiungere le labels alle mappe #5

--- frontend/src/presenters/MapPresenter.ts
+++ frontend/src/presenters/MapPresenter.ts
@@ -4,70 +4,70 @@ import type { Location } from "../models"
 import * as L from 'leaflet';
 
 export class MapPresenter {
-    rankedList: Writable<Promise<Location[]>> = writable(null);
-    map: any;
+  rankedList: Writable<Promise<Location[]>> = writable(null);
+  map: any;
 
-    constructor() {
-        this.resizeMap = this.resizeMap.bind(this);
-        this.initMap = this.initMap.bind(this);
-        this.rankedList.set(ResultsModel.getInstance().getRankedList(new Filter()));
-    }
+  constructor() {
+    this.resizeMap = this.resizeMap.bind(this);
+    this.initMap = this.initMap.bind(this);
+    this.rankedList.set(ResultsModel.getInstance().getRankedList(new Filter()));
+  }
 
-    createMap(container: any) {
-        let m = L.map(container).setView([45.420, 11.895], 13);
-        L.tileLayer(
-          'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-          {
-            attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
-              &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
-            subdomains: 'abcd',
-            maxZoom: 18,
-          }
-        ).addTo(m);
-    
-        return m;
+  createMap(container: any) {
+    let m = L.map(container).setView([45.420, 11.895], 13);
+    L.tileLayer(
+      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+      {
+        attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
+          &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
+        subdomains: 'abcd',
+        maxZoom: 18,
       }
+    ).addTo(m);
 
-    makeIcon() {
-		return new L.Icon({
-			iconUrl: './src/assets/icon.png',
-			iconSize: [40, 40],
-			iconAnchor: [20, 30], // [half of iconSize.x, 3/4 of iconSize.y] dependent on image used
-			popupAnchor: [-3, -76],
-			shadowUrl: null
-		});
+    return m;
+  }
+
+  makeIcon() {
+    return new L.Icon({
+      iconUrl: './src/assets/icon.png',
+      iconSize: [40, 40],
+      iconAnchor: [20, 30], // [half of iconSize.x, 3/4 of iconSize.y] dependent on image used
+      popupAnchor: [0, -30],
+      shadowUrl: null
+    });
 	}
 
-    createMarker(loc: [number, number]) {
+  createMarker(loc: [number, number]) {
 		let marker = L.marker(loc, {icon: this.makeIcon()});
 		return marker;
 	}
 
+  createPopup(name, score) {
+    return `<p>${name}</p>
+            <p>${score}</p>`;
+  }
+
   initMap(container: any) {
 		get(this.rankedList).then(locations => {
-        let markerPositions: [number, number][] = [];
-        let markerLayers: any;
-			  locations.forEach(location => {
-				  markerPositions.push([location.position.lat, location.position.long]);
-			  });
-
         this.map = this.createMap(container); 
+        let markerLayers = L.layerGroup();
         
-        markerLayers = L.layerGroup();
-        for(let position of markerPositions) {
-          let m = this.createMarker(position);
+			  locations.forEach(location => {
+				  let m = this.createMarker([location.position.lat, location.position.long]);
+          m.bindPopup(this.createPopup(location.name, location.score));
           markerLayers.addLayer(m);
-        }
+			  });
 
         markerLayers.addTo(this.map);
         
         return {
-                  destroy: () => { this.map.remove(); this.map=null; }
+          destroy: () => { this.map.remove(); this.map=null; }
         };
 		})
 	}
 
-    resizeMap() {
-        if(this.map) this.map.invalidateSize();
-    }
+  resizeMap() {
+    if(this.map) this.map.invalidateSize();
+  }
 }
