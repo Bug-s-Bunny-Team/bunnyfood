0d2701484e57c1a11355a841a561b1cd9d7aaa7b
mhetacc <marco.bello.vi@gmail.com>
2022-08-21 18:48:29 +0200

    faceScore=0 se nessun volto rilevato

--- backend/tests/test_face_scoring.py
+++ backend/tests/test_face_scoring.py
@@ -3,33 +3,39 @@ from pathlib import Path
 
 
 def scoreFaceRekognition():
-    with open(Path('MockupFiles/facialAnalysisMOC.json'), 'r') as f:
+    with open(Path('MockupFiles/facialAnalysisMOC_4.json'), 'r') as f:
         response = json.load(f)
 
     faceCount = 0
     scoreSum = 0
 
+    print(len(response['FaceDetails']))
 
-    for face in response['FaceDetails']:
-        pose = face['Pose']
-        #SE VOLTO DRITTO
-        if (abs(pose['Yaw']) <= 50) and (abs(pose['Pitch']) <= 50): #abs() perchè deve essere -50<pose<50
-            #CALCOLARE SCORE EMOZIONI
-            faceCount = faceCount + 1
-            faceSum = 0
-            disgusted = False
-            for emotion in face['Emotions']:
-                if emotion['Type'] == 'HAPPY':
-                    faceSum = faceSum + emotion['Confidence']
-                if emotion['Type'] == 'CALM':
-                    faceSum = faceSum + emotion['Confidence']*0.5 #ha peso minore di happy
-                if emotion['Type'] == 'DISGUSTED':
-                    if emotion['Confidence'] >= 50: #se disgust troppo elevato azzera il punteggio della faccia
-                        disgusted = True
-            if disgusted == False:
-                scoreSum = scoreSum + faceSum #se volto disgusted value >= allora face value = 0
-        #UN VOLTO STORTO VIENE IGNORATO NEL CALCOLO
-    totScore = scoreSum / faceCount
+    if len(response['FaceDetails']) > 0:
+        for face in response['FaceDetails']:
+            pose = face['Pose']
+            #SE VOLTO DRITTO
+            if (abs(pose['Yaw']) <= 50) and (abs(pose['Pitch']) <= 50): #abs() perchè deve essere -50<pose<50
+                #CALCOLARE SCORE EMOZIONI
+                faceCount = faceCount + 1
+                faceSum = 0
+                disgusted = False
+                for emotion in face['Emotions']:
+                    if emotion['Type'] == 'HAPPY':
+                        faceSum = faceSum + emotion['Confidence']
+                    if emotion['Type'] == 'CALM':
+                        faceSum = faceSum + emotion['Confidence']*0.5 #ha peso minore di happy
+                    if emotion['Type'] == 'DISGUSTED':
+                        if emotion['Confidence'] >= 50: #se disgust troppo elevato azzera il punteggio della faccia
+                            disgusted = True
+                if disgusted == False:
+                    scoreSum = scoreSum + faceSum #se volto disgusted value >= allora face value = 0
+            #UN VOLTO STORTO VIENE IGNORATO NEL CALCOLO
+        totScore = scoreSum / faceCount
+    else:
+        totScore = 0
+
+
     print('Facce analizzate=', faceCount)
     print('Score Immagine=' ,totScore)
 
