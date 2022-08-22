92eae51b4874d844241d3e33b0051f75f50a2186
Marco Volpato <marco@mvolpato.xyz>
2022-08-22 10:24:47 +0200

    api: customized docs

---
+++ backend/api/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1.0'
+
+API_TITLE = 'BunnyFood'
+API_DESCRIPTION = 'API for BunnyFood UI'
--- backend/api/main.py
+++ backend/api/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+
+from . import API_TITLE, API_DESCRIPTION, __version__
 
 from .routers import locations, profiles, preferences
 
@@ -7,3 +10,19 @@ app = FastAPI()
 app.include_router(locations.router)
 app.include_router(profiles.router)
 app.include_router(preferences.router)
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title=API_TITLE,
+        version=__version__,
+        description=API_DESCRIPTION,
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
