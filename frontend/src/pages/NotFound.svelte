7f99980e705a9ef0b88d9aedb66798efbcced53f
Angela <angyarena22@gmail.com>
2022-08-23 17:32:49 +0200

    page 404

--- frontend/src/pages/NotFound.svelte
+++ frontend/src/pages/NotFound.svelte
@@ -1,5 +1,34 @@
 <script lang="ts">
+
 </script>
 
+<div>
+    <p> <strong id="strong"> ERRORE! </strong> La pagina cercata non è stata trovata. </p>
+    <p> Riprova o <a href="./home"> CLICCA QUI </a> per essere reindirizzato alla pagina principale. </p>
+    <img class="img1" src="./src/pages/images/sad_salsiccia.jpg" alt="immagine di una salsiccia triste" />    
+    <img class="img2" src="./src/pages/images/sad_pizza.jpg" alt="immagine di una pizza triste" />    
+
+
+</div>
 <style>
+    p {
+        text-align: center;
+    }
+
+    #strong {
+        font-size: 35px;
+    }
+
+    .img1 {
+        float: left;
+        width: 30%;
+        padding: 2em;
+    }
+
+    .img2 {
+        float: right;
+        width: 40%;
+        padding: 2em;
+    }
+   
 </style>
