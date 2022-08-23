0bf7cf71638a84f92c4bd06af3f1da7b4455d7f1
Marco Volpato <marco@mvolpato.xyz>
2022-08-23 11:37:11 +0200

    scoring and sorting dummy

--- backend/functions/scoring/function/app.py
+++ backend/functions/scoring/function/app.py
@@ -1,16 +1,21 @@
-import json
-import os
+# import json
+# import os
+#
+# from .scoring_service import BasicScoringService
+# from .event_adapter import SQSEventAdapter, SNSEventAdapter
+# from .output_strategy import S3OutputStrategy, DBOutputStrategy
 
-from .scoring_service import BasicScoringService
-from .event_adapter import SQSEventAdapter, SNSEventAdapter
-from .output_strategy import S3OutputStrategy, DBOutputStrategy
+from common.utils import create_response
 
 
 def lambda_handler(event, context):
-    try:
-        scoringService = BasicScoringService(SNSEventAdapter(), DBOutputStrategy())
-        scoringService.score(event)
-        return 'Success'
-    except Exception as e:
-        print('Error processing Event {}'.format(json.dumps(event, indent=2)))
-        raise e
+    # try:
+    #     scoringService = BasicScoringService(SNSEventAdapter(), DBOutputStrategy())
+    #     scoringService.score(event)
+    #     return 'Success'
+    # except Exception as e:
+    #     print('Error processing Event {}'.format(json.dumps(event, indent=2)))
+    #     raise e
+
+    response = create_response('Hi from scoring', 200)
+    return response
--- backend/functions/sorter/function/app.py
+++ backend/functions/sorter/function/app.py
@@ -1,20 +1,16 @@
-import json
-
-from entity.post import Post
-from functions.sorter.function.sorting_service import SorterService
-
-
-def lambda_handler(event, post: Post):
-    try:
-        sorterEvent = SorterService
-        sorterEvent.sort(post)
-        return 'Success'
-    except Exception as e:
-        print('Error processing Event {}'.format(json.dumps(event, indent=2)))
-        raise e
+#
+# from entity.post import Post
+# from functions.sorter.function.sorting_service import SorterService
+from common.utils import create_response
 
 
-lambda_handler(
-    {},
-    None
-)
+def lambda_handler(event, context):
+    # try:
+    #     sorterEvent = SorterService
+    #     sorterEvent.sort(post)
+    #     return 'Success'
+    # except Exception as e:
+    #     print('Error processing Event {}'.format(json.dumps(event, indent=2)))
+    #     raise e
+
+    return create_response('Hi from sorter', 200)
