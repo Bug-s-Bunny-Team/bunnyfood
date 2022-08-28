5e3fb29a27b2f452efcd8a04538eee84f42450fe
Marco Volpato <marco@mvolpato.xyz>
2022-08-28 21:05:08 +0200

    scraping: add info logging

--- backend/functions/scraping/function/app.py
+++ backend/functions/scraping/function/app.py
@@ -5,7 +5,10 @@ from .utils import create_service, create_scraper
 def lambda_handler(event, context):
     event = ScrapingEvent(**event)
 
+    print(event.json())
+
     scraper = create_scraper(event.scraper)
+    print(f'using {scraper.__class__.__name__}')
     service = create_service(scraper)
 
     return service.process_event(event)
