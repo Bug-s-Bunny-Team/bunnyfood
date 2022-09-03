25694dbf607fd9333a4c255913f751f2ac4c2cd4
difanta <jackiedifant@gmail.com>
2022-09-03 17:58:25 +0200

    .

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  collectCoverage: true,
   verbose: true,
   setupFiles: [
     "./jest.environment.js"
   ],
+  collectCoverage: true,
   collectCoverageFrom: ["./src/models/*", "./src/presenters/*", "./src/utils.ts", "./src/models.ts"]
 };
