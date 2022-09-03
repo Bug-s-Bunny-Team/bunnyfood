7636bfd25c714d40dcebf207b979e8633cf3b466
difanta <jackiedifant@gmail.com>
2022-09-03 17:56:36 +0200

    test coverage

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
+  collectCoverage: true,
   verbose: true,
   setupFiles: [
     "./jest.environment.js"
   ],
+  collectCoverageFrom: ["./src/models/*", "./src/presenters/*", "./src/utils.ts", "./src/models.ts"]
 };
