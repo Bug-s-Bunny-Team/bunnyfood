2849948e25d2a61b24907a46f2db11880436e31a
Marco Volpato <marco@mvolpato.xyz>
2022-09-03 16:59:18 +0200

    tests: add coverage reporting

--- backend/tests/api/test_utils.py
+++ backend/tests/api/test_utils.py
@@ -1,12 +1,16 @@
+import pytest
+
 from api.utils import search_social_profile
 
 
+@pytest.mark.vcr
 def test_search_profile_existing():
     exists = search_social_profile('antoniorazzi')
 
     assert exists
 
 
+@pytest.mark.vcr
 def test_search_profile_not_existing():
     exists = search_social_profile('thisdoesnotexistsblaudbsui384')
 
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from database import populate_db
+from .database import populate_db
 from db import create_connection_url, models
 
 
